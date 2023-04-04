1b5f167b0d01dc81c82ead5d44eab88b5f93495f
Konstantin Leschenko <klav28@gmail.com>
2023-04-04 08:04:55 +0300

    simple box upd, svg rmv

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 import { PixabayAPI } from "./pixabay_api";
 import Notiflix from "notiflix";
 import createPhotoGallery from '../templates/image-card.hbs';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const pixabayApi = new PixabayAPI();
 const PER_PAGE = 40;
@@ -8,6 +10,7 @@ const PER_PAGE = 40;
 const elSearchForm = document.querySelector("#search-form");
 const elGallery = document.querySelector(".gallery");
 const elLoadMoreBtn = document.querySelector(".load-more__button");
+let lightbox = null;
 
 const handleFormSubmit = async ev => {
   ev.preventDefault();
@@ -40,6 +43,11 @@ const handleFormSubmit = async ev => {
 
     elGallery.innerHTML = createPhotoGallery(data.hits).replaceAll("_640.", "_340.");
 
+    if (lightbox !== null) {
+      lightbox.refresh();
+    } else {
+      lightbox = new SimpleLightbox('.gallery a', {captions: true, captionDelay:250, captionsData:"alt"});
+    }
 
 
     if (data.totalHits < PER_PAGE) {
@@ -68,6 +76,8 @@ const handleLoadMoreBtnClick = async () => {
       'beforeend',
       createPhotoGallery(data.hits).replaceAll("_640.", "_340.")
     );
+      lightbox.refresh();
+      
   } catch (err) {
     console.log(err);
   }
