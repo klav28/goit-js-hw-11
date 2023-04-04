a01cb53979141fd60f634bda4c472f6129fe8e7b
klav28 <klav28@gmail.com>
2023-04-04 18:53:15 +0300

    markup upd

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import Notiflix from "notiflix";
 import createPhotoGallery from '../templates/image-card.hbs';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+import OnlyScroll from 'only-scrollbar';
 
 const pixabayApi = new PixabayAPI();
 const PER_PAGE = 40;
@@ -11,6 +12,8 @@ const elSearchForm = document.querySelector("#search-form");
 const elGallery = document.querySelector(".gallery");
 const elLoadMoreBtn = document.querySelector(".load-more__button");
 let lightbox = null;
+let scroll = null;
+
 
 const handleFormSubmit = async ev => {
   ev.preventDefault();
@@ -31,8 +34,6 @@ const handleFormSubmit = async ev => {
   try {
     const { data } = await pixabayApi.fetchPhotos();
 
-    console.log(data);
-
     if (!data.total) {
       Notiflix.Notify.failure("Images not found");
       elSearchForm.reset();
@@ -47,6 +48,7 @@ const handleFormSubmit = async ev => {
       lightbox.refresh();
     } else {
       lightbox = new SimpleLightbox('.gallery a', { captions: true, enableKeyboard: true, captionDelay: 250, captionsData: "alt" });
+      scroll = new OnlyScroll(document.querySelector('.gallery'));
     }
 
     if (data.totalHits < PER_PAGE) {
@@ -56,12 +58,12 @@ const handleFormSubmit = async ev => {
       elLoadMoreBtn.classList.remove('is-hidden');
     }
 
-
-
   } catch (err) {
     console.log(err);
   }
 };
+
+
 
 const handleLoadMoreBtnClick = async () => {
   pixabayApi.page += 1;
